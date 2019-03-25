10b4ba06931022f72e78adc53cdd09ca76690f52
koros99 <koronje254@gmail.com>
2019-03-25 12:07:00 +0300

    Add date, likes and unlikes property to constructor

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -4,5 +4,6 @@ export class Quote {
   constructor(public id: number, public body: string, public author: string, public publisher:string, public date: Date, public likes: number, public unlikes: number){
     this.showAuthor=false;
     this.showPublisher=false;
+    
   }
 }
