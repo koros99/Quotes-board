321de218045194be5ad3efcc01a52440c36465f4
koros99 <koronje254@gmail.com>
2019-03-25 06:43:00 +0300

    Add forms component, with functions to allow users to add quotes

---
+++ src/app/quotes-form/quotes-form.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {Quote} from '../quote'
+
+@Component({
+  selector: 'app-quotes-form',
+  templateUrl: './quotes-form.component.html',
+  styleUrls: ['./quotes-form.component.css']
+})
+export class QuotesFormComponent implements OnInit {
+
+  newQuote = new Quote(0,"","","");
+  @Output() addQuote= new EventEmitter<Quote>();
+
+  submitQuote(){
+    this.addQuote.emit(this.newQuote);
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
